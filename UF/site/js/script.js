704d160b571ff36e0bfc2acaae15c8387e9c5e52
Vlad <martymqfly@gmail.com>
2016-12-05 17:28:45 +0200

    last updates

---
+++ UF/site/js/script.js
@@ -0,0 +1,353 @@
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+/* cookie.JS
+ ========================================================*/
+include('js/jquery.cookie.js');
+
+/* Easing library
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+/* PointerEvents
+ ========================================================*/
+;
+(function ($) {
+    if(isIE() && isIE() < 11){
+        include('js/pointer-events.js');
+        $('html').addClass('lt-ie11');
+        $(document).ready(function(){
+            PointerEventsPolyfill.initialize({});
+        });
+    }
+})(jQuery);
+
+/* Stick up menus
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/tmstickup.js');
+
+        $(document).ready(function () {
+            $('#stuck_container').TMStickUp({})
+        });
+    }
+})(jQuery);
+
+/* ToTop
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.ui.totop.js');
+
+        $(document).ready(function () {
+            $().UItoTop({
+                easingType: 'easeOutQuart',
+                containerClass: 'toTop fa fa-angle-up'
+            });
+        });
+    }
+})(jQuery);
+
+/* EqualHeights
+ ========================================================*/
+;
+(function ($) {
+    var o = $('[data-equal-group]');
+    if (o.length > 0) {
+        include('js/jquery.equalheights.js');
+    }
+})(jQuery);
+
+/* Copyright Year
+ ========================================================*/
+;
+(function ($) {
+    var currentYear = (new Date).getFullYear();
+    $(document).ready(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+
+/* Superfish menus
+ ========================================================*/
+;
+(function ($) {
+    include('js/superfish.js');
+})(jQuery);
+
+/* Google Map
+ ========================================================*/
+;
+(function ($) {
+    var o = document.getElementById("google-map");
+    if (o) {
+        include('//maps.google.com/maps/api/js?sensor=false');
+        include('js/jquery.rd-google-map.js');
+
+        $(document).ready(function () {
+            var o = $('#google-map');
+            if (o.length > 0) {
+                o.googleMap();
+            }
+        });
+    }
+})
+(jQuery);
+
+/* WOW
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+
+    if ((navigator.userAgent.toLowerCase().indexOf('msie') == -1 ) || (isIE() && isIE() > 9)) {
+        if (o.hasClass('desktop')) {
+            include('js/wow.js');
+
+            $(document).ready(function () {
+                new WOW().init();
+            });
+        }
+    }
+})(jQuery);
+
+/* Mailform
+=============================================*/
+;(function ($) {
+    include('js/mailform/jquery.form.min.js');
+    include('js/mailform/jquery.rd-mailform.min.js');
+})(jQuery);
+
+/* Orientation tablet fix
+ ========================================================*/
+$(function () {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menus li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+var ua = navigator.userAgent.toLocaleLowerCase(),
+    regV = /ipod|ipad|iphone/gi,
+    result = ua.match(regV),
+    userScale = "";
+if (!result) {
+    userScale = ",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0' + userScale + '">');
+
+/* Owl Carousel
+========================================================*/
+;(function ($) {
+    var o = $('.product-carousel');
+    if (o.length > 0) {
+        include('js/owl.carousel.min.js');
+        $(document).ready(function () {
+            o.owlCarousel({
+                margin: 4,
+                smartSpeed: 450,
+                loop: true,
+                dots: true,
+                dotsEach: 1,
+                nav: true,
+                navClass: ['owl-prev', 'owl-next'],
+                responsive: {
+                    0: { items: 1 },
+                    640: { items: 3},
+                    768: { items: 3},
+                    960: { items: 3},
+                    980: { items: 3}
+                }
+            });
+        });
+    }
+})(jQuery);
+
+;(function ($) {
+    var o = $('.dealer-carousel');
+    if (o.length > 0) {
+        include('js/owl.carousel.min.js');
+        $(document).ready(function () {
+            o.owlCarousel({
+                margin: 30,
+                smartSpeed: 450,
+                loop: true,
+                dots: true,
+                dotsEach: 1,
+                nav: true,
+                navClass: ['owl-prev', 'owl-next'],
+                responsive: {
+                    0: { items: 1 },
+                    639: { items: 1},
+                    768: { items: 4},
+                    980: { items: 4},
+                    1200: { items: 4}
+                }
+            });
+        });
+    }
+})(jQuery);
+
+/* Responsive Tabs
+ ========================================================*/
+;
+(function ($) {
+    var o = $('.resp-tabs');
+    if (o.length > 0) {
+        include('js/jquery.responsive.tabs.js');
+
+        $(document).ready(function () {
+            o.easyResponsiveTabs();
+        });
+    }
+})(jQuery);
+
+
+/* Responsive Gallery
+ ========================================================*/
+    (function($) {
+        $(function() {
+
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+
+            });
+
+
+        });
+    })(jQuery);
+
+/* Shoping cart
+ ========================================================*/
+
+$('.basket-table .close ,.drop .close').on('click', function (e) {
+    $(this).closest('tr').find("td").slideUp(300);
+     return false;
+});
+
+
+    quantitySwitch();
+    function quantitySwitch(){
+      $(document).on('click','.amount .minus,.amount .plus',function(e){
+        var input=$(this).parent().find('input'),
+          val=parseInt(input.val());
+        if($(this).is('.plus')){
+          input.val((++val))
+        }else{
+          if(val-1<=1)
+            input.val("1")
+          else
+            input.val((--val)+"")
+        }
+        e.preventDefault();
+      });
+    };
+
+  $(document).ready(function  () {
+    $('.mob-category__btn').click(function  () {
+        $('.mob-category__list').slideToggle('medium');
+    });
+});
+
+
+
+/* City drop menu
+ ========================================================*/
+
+ $(function(){
+
+     $('.main-item-1').click(function(){
+     $(".cities_list").slideToggle('fast');
+     });
+     $('ul.cities_list li').click(function(){
+     var tx = $(this).html();
+     // var tv = $(this).attr('alt');
+     // $(".cities_list").slideUp('fast');
+     $(".city_name").html(tx);
+     // $(".delivery_text").html(tv);
+     });
+ })
+
+/* City drop menu
+ ========================================================*/
+ $(".toggle-menu").click(function () {
+        $(this).toggleClass("active_menu");
+ });
+
+
+ /* Modal
+ ========================================================*/
+$(document).ready(function() { // зaпускaем скрипт пoсле зaгрузки всех элементoв
+    /* зaсунем срaзу все элементы в переменные, чтoбы скрипту не прихoдилoсь их кaждый рaз искaть при кликaх */
+    var overlay = $('#overlay'); // пoдлoжкa, дoлжнa быть oднa нa стрaнице
+    var open_modal = $('.open_modal'); // все ссылки, кoтoрые будут oткрывaть oкнa
+    var close = $('.modal_close, #overlay'); // все, чтo зaкрывaет мoдaльнoе oкнo, т.е. крестик и oверлэй-пoдлoжкa
+    var modal = $('.modal_div'); // все скрытые мoдaльные oкнa
+
+     open_modal.click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal
+         event.preventDefault(); // вырубaем стaндaртнoе пoведение
+         var div = $(this).attr('href'); // вoзьмем стрoку с селектoрoм у кликнутoй ссылки
+         overlay.fadeIn(400, //пoкaзывaем oверлэй
+             function(){ // пoсле oкoнчaния пoкaзывaния oверлэя
+                 $(div) // берем стрoку с селектoрoм и делaем из нее jquery oбъект
+                     .css('display', 'block')
+                     .animate({opacity: 1, top: '50%'}, 200); // плaвнo пoкaзывaем
+         });
+     });
+
+     close.click( function(){ // лoвим клик пo крестику или oверлэю
+            modal // все мoдaльные oкнa
+             .animate({opacity: 0, top: '45%'}, 200, // плaвнo прячем
+                 function(){ // пoсле этoгo
+                     $(this).css('display', 'none');
+                     overlay.fadeOut(400); // прячем пoдлoжку
+                 }
+             );
+     });
+});
+
+
